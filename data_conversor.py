3500393dc4c715737d5fbb2b54f3767cfd17c7cb
Asteris <94953985+AsterisCrack@users.noreply.github.com>
2024-02-01 18:40:25 +0100

    Separated data conversor class in new file

---
+++ data_conversor.py
@@ -0,0 +1,31 @@
+from DataConversors import dfToJson, dfToAvro, dfToCsv, dfToParquet, dfToExcel#, dfToOrc
+
+class DataConversors:
+    # Constructor of the class
+    def __init__(self, df):
+        self.df = df
+
+    # json conversion
+    def toJson(self, fileName) -> None:
+        dfToJson.dfToJson(self.df, fileName)
+
+    # avro conversion
+    def toAvro(self, fileName) -> None:
+        dfToAvro.dfToAvro(self.df, fileName)
+
+    # csv conversion
+    def toCsv(self, fileName) -> None:
+        dfToCsv.dfToCsv(self.df, fileName)
+    
+    # parquet conversion
+    def toParquet(self, fileName) -> None:
+        dfToParquet.dfToParquet(self.df, fileName)
+
+    # excel conversion
+    def toExcel(self, fileName) -> None:
+        dfToExcel.dfToExcel(self.df, fileName)
+
+    # orc conversion
+    def toOrc(self, fileName) -> None:
+        #dfToOrc.dfToOrc(self.df, fileName)
+        pass
--- main.py
+++ main.py
@@ -1,42 +1,10 @@
-from DataConversors import dfToJson, dfToAvro, dfToCsv, dfToParquet, dfToExcel#, dfToOrc
+from data_conversor import DataConversors
 import get_historical_data as DataExtractor
 import datetime as dt
 import sys, os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-class DataConversors:
-
-    # Constructor of the class
-    def __init__(self, df):
-        self.df = df
-
-    # json conversion
-    def toJson(self, fileName) -> None:
-        dfToJson.dfToJson(self.df, fileName)
-
-    # avro conversion
-    def toAvro(self, fileName) -> None:
-        dfToAvro.dfToAvro(self.df, fileName)
-
-    # csv conversion
-    def toCsv(self, fileName) -> None:
-        dfToCsv.dfToCsv(self.df, fileName)
-    
-    # parquet conversion
-    def toParquet(self, fileName) -> None:
-        dfToParquet.dfToParquet(self.df, fileName)
-
-    # excel conversion
-    def toExcel(self, fileName) -> None:
-        dfToExcel.dfToExcel(self.df, fileName)
-
-    # orc conversion
-    def toOrc(self, fileName) -> None:
-        #dfToOrc.dfToOrc(self.df, fileName)
-        pass
-
     
 def main(year, fileTypes):
     # Get the start date
@@ -96,7 +64,3 @@ if __name__ == "__main__":
     
     for year in years:
         main(year, file_types)
-
-
-
-        
